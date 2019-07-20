326646a79b7721aaaaa1ca66ed17f8b14f14a145
AlMuz <almuz@inbox.lv>
2019-07-20 15:24:23 +0300

    Creating Stocks Components

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,14 +1,33 @@
 <template>
-  <div>
-    <h1>
-      The Stock - Stock component
-    </h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+        {{ name }}
+        
+        <small>(Price: {{ price }})</small>
+      </h3>
+      </div>
+    </div>
+    <div class="panel-body">
+      <div class="pull-left">
+        <input type="number" class="form-control" placeholder="Quantity">
+      </div>
+      <div class="pull-right">
+        <button class="btn btn-success">Buy</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        name: '',
+        price: 0,
+      }
+    }
   }
 </script>
 
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,14 +1,27 @@
 <template>
   <div>
-    <h1>
-      The Stock - Stocks component
-    </h1>
+    <Stock v-for="stock in stocks">
+      
+    </Stock>
   </div>
 </template>
 
 <script>
+  import Stock from './Stock.vue';
   export default {
-
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'BMW', price: 100 },
+          { id: 1, name: 'Google', price: 200 },
+          { id: 1, name: 'Apple', price: 50 },
+          { id: 1, name: 'Twitter', price: 11 },
+        ]
+      }
+    },
+    components: {
+      Stock,
+    }
   }
 </script>
 
