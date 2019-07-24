0f5d6895eeb7d19260b05b6ca4ff8d1be5bc6b78
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-07-24 11:00:18 +0300

    Setting up the Vuex State Management

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -9,18 +9,13 @@
 <script>
   import Stock from './Stock.vue';
   export default {
-    data() {
-      return {
-        stocks: [
-          { id: 1, name: 'BMW', price: 100 },
-          { id: 1, name: 'Google', price: 200 },
-          { id: 1, name: 'Apple', price: 50 },
-          { id: 1, name: 'Twitter', price: 11 },
-        ]
-      }
-    },
     components: {
       Stock,
+    },
+    computed: {
+        stocks() {
+          return this.$store.getters.stocks;
+        }
     }
   }
 </script>
