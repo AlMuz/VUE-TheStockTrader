139031c44d6e440a440fb3958d9fb37821344c8b
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-07-24 13:17:01 +0300

    vue-resource added

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+import VueResource from 'vue-resource'
 
+Vue.use(VueResource);
 Vue.config.productionTip = false
+Vue.http.options.root = process.env.VUE_APP_FIREBASE_URL;
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
